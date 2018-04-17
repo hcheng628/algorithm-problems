440e77f48a4db588ffdca96ed9c8e89bfffd166e
supercheng <chenghongyu628@gmail.com>
2018-04-17 12:33:14 -0400

    init project and add prev execs

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/palindromenumber/App.java
@@ -0,0 +1,23 @@
+package us.supercheng.algorithm.problems.palindromenumber;
+
+public class App {
+
+    public static void main(String[]  args) {
+        App app = new App();
+        System.out.println(app.isPalindrome(52343221));
+    }
+
+    public boolean isPalindrome(int x) {
+        if(x<0) {
+            return false;
+        }
+
+        String str = x + "";
+        for(int i=0;i<str.length()/2;i++){
+            if (str.charAt(i) != str.charAt(str.length() - i -1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/romantoint/App.java
@@ -0,0 +1,79 @@
+package us.supercheng.algorithm.problems.romantoint;
+
+public class App {
+
+    public static void main(String[]  args) {
+        App app = new App();
+        System.out.println(app.romanToInt("CDXX"));
+    }
+
+    public int romanToInt(String s) {
+        int intVal = 0;
+        char c = ',';
+        char nextC = ',';
+        int len = s.length();
+        for(int i =0; i<len;i++) {
+            c = s.charAt(i);
+            if((i+1) < len){
+                nextC = s.charAt(i + 1);
+            }
+            switch(c) {
+                case 'I':
+                    switch(nextC) {
+                        case 'V':
+                            intVal += 4;
+                            i++;
+                            break;
+                        case 'X':
+                            intVal += 9;
+                            i++;
+                            break;
+                        default:
+                            intVal += 1;
+                    }
+                    break;
+                case 'V':
+                    intVal += 5;
+                    break;
+                case 'X':
+                    switch(nextC) {
+                        case 'L':
+                            intVal += 40;
+                            i++;
+                            break;
+                        case 'C':
+                            intVal += 90;
+                            i++;
+                            break;
+                        default:
+                            intVal += 10;
+                    }
+                    break;
+                case 'L':
+                    intVal += 50;
+                    break;
+                case 'C':
+                    switch(nextC) {
+                        case 'D':
+                            intVal += 400;
+                            i++;
+                            break;
+                        case 'M':
+                            intVal += 900;
+                            i++;
+                            break;
+                        default:
+                            intVal += 100;
+                    }
+                    break;
+                case 'D':
+                    intVal += 500;
+                    break;
+                case 'M':
+                    intVal += 1000;
+                    break;
+            }
+        }
+        return intVal;
+    }
+}
