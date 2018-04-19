8dd62b090215b26e25df917ba701d5905274dafc
supercheng <chenghongyu628@gmail.com>
2018-04-19 17:02:35 -0400

    add more problems

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/common/entity/IntNode.java
@@ -0,0 +1,42 @@
+package us.supercheng.algorithm.common.entity;
+
+public class IntNode {
+    private Integer val;
+    private IntNode node;
+
+    public IntNode() {
+        this.val = null;
+        this.node = null;
+    }
+
+    public Integer getVal() {
+        return val;
+    }
+
+    public void setVal(Integer val) {
+        this.val = val;
+    }
+
+    public IntNode getNode() {
+        return node;
+    }
+
+    public void setNode(IntNode node) {
+        this.node = node;
+    }
+
+    @Override
+    public String toString() {
+        return "IntNode{" +
+                "val=" + val +
+                '}';
+    }
+
+    public void printList(IntNode node){
+        while (node != null) {
+            System.out.print(node + " ");
+            node = node.getNode();
+        }
+        System.out.println();
+    }
+}
