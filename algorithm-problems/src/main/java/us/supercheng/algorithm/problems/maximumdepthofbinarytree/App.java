dfdedac6131ade62d8f546e8e2b1675d2724f74d
supercheng <chenghongyu628@gmail.com>
2018-05-06 11:32:52 -0400

    add maxDepthBinaryTree

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximumdepthofbinarytree/App.java
@@ -0,0 +1,34 @@
+package us.supercheng.algorithm.problems.maximumdepthofbinarytree;
+
+public class App {
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+    }
+
+    public static void main(String[]  args) {
+        App app = new App();
+        System.out.println(app.maxDepth(null));
+    }
+
+    public int maxDepth(TreeNode root) {
+        Integer count = 0;
+        if(root == null) {
+            return count;
+        }
+        count = goDown(root, count);
+        return count;
+    }
+
+    public Integer goDown(TreeNode t, Integer count) {
+        if(t == null) {
+            return count;
+        }
+        count++;
+        int countA = goDown(t.left, count);
+        int countB = goDown(t.right, count);
+        return countA > countB ? countA : countB;
+    }
+}
