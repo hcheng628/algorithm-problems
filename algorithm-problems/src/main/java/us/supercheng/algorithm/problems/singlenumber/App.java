09059cdaf006c9a7011464a61a676c44599e6a79
supercheng <chenghongyu628@gmail.com>
2018-05-17 20:29:35 -0400

    add different solutions

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/singlenumber/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/singlenumber/App.java
@@ -1,12 +1,14 @@
 package us.supercheng.algorithm.problems.singlenumber;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
         App app = new App();
         int [] arr = {2,1,3,4,3,1,2,4,5,6};
-        int a = app.singleNumber(arr);
+        int a = app.singleNumber2(arr);
         System.out.println("Final Result: " + a);
     }
 
@@ -19,5 +21,17 @@ public class App {
             }
         }
         return nums[nums.length-1];
+    }
+
+    public int singleNumber2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++) {
+            if(set.contains(nums[i])) {
+                set.remove(nums[i]);
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return set.iterator().next();
     }
 }
--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validpalindrome/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validpalindrome/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
-        app.isPalindrome("I think this is working");
+        System.out.println(app.isPalindrome2("I think this is working"));
     }
 
     public boolean isPalindrome(String s) {
@@ -23,6 +23,29 @@ public class App {
         for (int i=0;i<index;i++) {
             if(s.charAt(i) != s.charAt(s.length() - 1 -i)) {
                 return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome2(String s) {
+        int head = 0;
+        int tail = s.length() -1;
+        while(head < tail + 1) {
+            char charHead = s.charAt(head);
+            char charTail = s.charAt(tail);
+            if(Character.isLetterOrDigit(charHead)){
+                if(Character.isLetterOrDigit(charTail)) {
+                    if(Character.toLowerCase(charHead) != Character.toLowerCase(charTail)) {
+                        return false;
+                    }
+                    tail--;
+                    head++;
+                } else {
+                    tail--;
+                }
+            } else {
+                head++;
             }
         }
         return true;
