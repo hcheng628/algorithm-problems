17088c0a390565256a2bf1a5c922e93caef978b9
supercheng <chenghongyu628@gmail.com>
2018-06-22 10:26:46 -0400

    constructtherectangle

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/constructtherectangle/App.java
@@ -0,0 +1,18 @@
+package us.supercheng.algorithm.problems.constructtherectangle;
+
+import us.supercheng.algorithm.common.helper.PrintHelper;
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        PrintHelper.echoLn(Arrays.toString(new App().constructRectangle(10001)));
+    }
+
+    public int[] constructRectangle(int area) {
+        int len = (int)(Math.sqrt(area));
+        while(area % len != 0)
+            --len;
+        return new int[] {area/len, len};
+    }
+}
