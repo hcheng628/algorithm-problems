47969e1fb7519fcdc0b73a950c5d4dbd45efb82d
supercheng <chenghongyu628@gmail.com>
2018-06-29 12:43:09 -0400

    add reversestringii + studentattendancerecordi

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/reversestringii/App.java
@@ -0,0 +1,43 @@
+package us.supercheng.algorithm.problems.reversestringii;
+
+import us.supercheng.algorithm.common.helper.PrintHelper;
+
+public class App {
+
+    public static void main(String[] args) {
+        String s = "abcdefg";
+        int k = 4;
+        PrintHelper.echoLn(new App().reverseStr(s, k));
+    }
+
+    public String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+
+        if(chars.length <= k) {
+            this.reverseChars(chars, 0, chars.length -1);
+            return String.valueOf(chars);
+        }
+
+        boolean oddFlag = true;
+        int index = 0;
+        while(index + k <= chars.length) {
+            if(oddFlag)
+                this.reverseChars(chars, index, index+(k-1));
+            oddFlag = !oddFlag;
+            index += k;
+        }
+        if(index > chars.length)
+            this.reverseChars(chars, (index - k), chars.length -1);
+        else if(index < chars.length && oddFlag)
+            this.reverseChars(chars, index, chars.length -1);
+        return String.valueOf(chars);
+    }
+
+    private void reverseChars(char [] chars, int left, int right) {
+        while(right>left) {
+            char temp = chars[left];
+            chars[left++] = chars[right];
+            chars[right--] = temp;
+        }
+    }
+}
