4574001e055ab98323daefe42165649704378d4a
supercheng <chenghongyu628@gmail.com>
2018-07-02 16:50:55 -0400

    add subtreeofanothertree

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/subtreeofanothertree/App.java
@@ -0,0 +1,30 @@
+package us.supercheng.algorithm.problems.subtreeofanothertree;
+
+import us.supercheng.algorithm.common.entity.TreeNode;
+
+public class App {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s.val == t.val) {
+            boolean rootCheck = this.helper(s, t);
+            if(rootCheck)
+                return true;
+        }
+        boolean left = s.left != null ? this.isSubtree(s.left,  t) : false;
+        boolean right = s.right != null ? this.isSubtree(s.right,  t) : false;
+        return left || right;
+    }
+
+    private boolean helper(TreeNode s, TreeNode t) {
+        if(s == null && t == null)
+            return true;
+        else if ((s != null && t == null) || (s == null && t != null))
+            return false;
+        else {
+            if(s.val != t.val)
+                return false;
+            else
+                return this.helper(s.left, t.left) && this.helper(s.right, t.right);
+        }
+    }
+}
