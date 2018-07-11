a75226890d00d114e13a731058618d45ac0b2450
supercheng <chenghongyu628@gmail.com>
2018-07-11 16:33:27 -0400

    add maximumproductofthreenumbers + mergetwobinarytrees

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximumproductofthreenumbers/App.java
@@ -0,0 +1,10 @@
+package us.supercheng.algorithm.problems.maximumproductofthreenumbers;
+
+import java.util.Arrays;
+
+public class App {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return  Math.max(nums[nums.length-3] * nums[nums.length-2] * nums[nums.length-1], nums[0] * nums[1] * nums[nums.length-1]);
+    }
+}
