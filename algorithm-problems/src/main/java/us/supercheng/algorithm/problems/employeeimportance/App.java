3455183097e25b07d067e2f8a12a681d654dba2d
supercheng <chenghongyu628@gmail.com>
2018-07-27 15:45:41 -0400

    add different algorithm implementations

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/employeeimportance/App.java
@@ -0,0 +1,73 @@
+package us.supercheng.algorithm.problems.employeeimportance;
+
+import java.util.*;
+
+public class App {
+
+    class Employee {
+        // It's the unique id of each node;
+        // unique id of this employee
+        public int id;
+        // the importance value of this employee
+        public int importance;
+        // the id of direct subordinates
+        public List<Integer> subordinates;
+    };
+
+    public int getImportance3(List<Employee> employees, int id) {
+        Map<Integer, Employee> empValMap = new HashMap<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int sum = 0;
+        for(Employee each : employees) {
+            empValMap.put(each.id, each);
+            if(each.id == id) {
+                for(int eachSub : each.subordinates)
+                    queue.add(eachSub);
+                sum = each.importance;
+            }
+        }
+        while(queue.size() > 0) {
+            Integer subEmpId = queue.poll();
+            Employee cur = empValMap.get(subEmpId);
+            sum += cur.importance;
+            for(int eachSubEmpId : cur.subordinates)
+                queue.add(eachSubEmpId);
+        }
+        return sum;
+    }
+
+    public int getImportance2(List<Employee> employees, int id) {
+        Employee [] bucket = new Employee[2000];
+        Queue<Integer> queue = new LinkedList<>();
+        int sum = 0;
+        for(Employee each : employees) {
+            bucket[each.id] = each;
+            if(each.id == id) {
+                for(int eachSub : each.subordinates)
+                    queue.add(eachSub);
+                sum = each.importance;
+            }
+        }
+        while(queue.size() > 0) {
+            Employee cur = bucket[queue.poll()];
+            sum += cur.importance;
+            for(int eachSubEmpId : cur.subordinates)
+                queue.add(eachSubEmpId);
+        }
+        return sum;
+    }
+
+    public int getImportance(List<Employee> employees, int id) {
+        Employee [] bucket = new Employee[2000];
+        for(Employee each : employees)
+            bucket[each.id] = each;
+        return managerImportance(bucket, id);
+    }
+
+    private int managerImportance(Employee [] bucket, int empId) {
+        int totalVal = bucket[empId].importance;
+        for(int subEmpId : bucket[empId].subordinates)
+            totalVal += managerImportance(bucket, subEmpId);
+        return totalVal;
+    }
+}
--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/searchinabinarysearchtree/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/searchinabinarysearchtree/App.java
@@ -6,7 +6,7 @@ public class App {
 
     TreeNode node = null;
 
-    public TreeNode searchBST(TreeNode root, int val) {
+    public TreeNode searchBST1(TreeNode root, int val) {
         helper(root, val);
         return node;
     }
@@ -21,5 +21,17 @@ public class App {
             this.helper(root.left, val);
         else
             this.helper(root.right, val);
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null)
+            return root;
+
+        if(val > root.val)
+            return searchBST(root.right, val);
+        else if (val < root.val)
+            return searchBST(root.left, val);
+        else
+            return root;
     }
 }
