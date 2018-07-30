7c26b033d5cd6632fd98d00704750acec028babd
supercheng <chenghongyu628@gmail.com>
2018-07-30 11:31:24 -0400

    add kthlargestelementinastream

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/kthlargestelementinastream/App.java
@@ -0,0 +1,30 @@
+package us.supercheng.algorithm.problems.kthlargestelementinastream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class App {
+
+    private int kth;
+    private ArrayList<Integer> list;
+
+    public App(int k, int[] nums) {
+        this.kth = k - 1;
+        this.list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+            this.add(nums[i]);
+    }
+
+    public int add(int val) {
+        int index = Collections.binarySearch(this.list, val);
+        if(index > -1)
+            this.list.add(index, val);
+        else
+            this.list.add(-1*(index+1), val);
+        int count = this.list.size() - 1 - this.kth;
+        if(count >= 0 && count <= this.list.size()-1)
+            return this.list.get(count);
+        else
+            return this.list.get(0);
+    }
+}
