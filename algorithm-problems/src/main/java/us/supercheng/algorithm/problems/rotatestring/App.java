c498aab3664a2aec890777855dfeb1bd05ec33c4
supercheng <chenghongyu628@gmail.com>
2018-08-10 16:58:41 -0400

    add rotate-string

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/rotatestring/App.java
@@ -0,0 +1,25 @@
+package us.supercheng.algorithm.problems.rotatestring;
+
+import us.supercheng.algorithm.common.helper.PrintHelper;
+
+public class App {
+
+    public static void main(String[] args) {
+        String A = "abcde", B = "cdeab";
+        PrintHelper.echoLn(new App().rotateString(A, B));
+    }
+
+    public boolean rotateString(String A, String B) {
+        if(A == null && B == null || A.length() == 0 && B.length() == 0)
+            return true;
+        if(A.length() != B.length() || A == null || B == null)
+            return false;
+
+        for(int i=0;i<A.length()-1;i++) {
+            A = A.substring(1,A.length()) + A.charAt(0);
+            if(B.contains(A))
+                return true;
+        }
+        return false;
+    }
+}
