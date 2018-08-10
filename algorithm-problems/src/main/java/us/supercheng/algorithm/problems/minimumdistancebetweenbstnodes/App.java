2d7f63c0fa10018e4e16bee5460c2eea2bcd33b1
supercheng <chenghongyu628@gmail.com>
2018-08-10 16:34:11 -0400

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/minimumdistancebetweenbstnodes/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/minimumdistancebetweenbstnodes/App.java
@@ -2,15 +2,13 @@ package us.supercheng.algorithm.problems.minimumdistancebetweenbstnodes;
 
 import us.supercheng.algorithm.common.entity.TreeNode;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class App {
 
-    public int minDiffInBST(TreeNode root) {
+    public int minDiffInBST1(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        this.helper(root, list);
-        Collections.sort(list);
+        this.helper1(root, list);
         int ret = Integer.MAX_VALUE;
         for(int i=list.size()-1;i>0;i--) {
             int res = list.get(i) - list.get(i-1);
@@ -20,11 +18,34 @@ public class App {
         return ret;
     }
 
-    private void helper(TreeNode root, List<Integer> list) {
+    private void helper1(TreeNode root, List<Integer> list) {
         if(root == null)
             return;
+        helper1(root.left, list);
         list.add(root.val);
-        helper(root.left, list);
-        helper(root.right, list);
+        helper1(root.right, list);
+    }
+
+
+    Integer prev = null;
+    int ret = Integer.MAX_VALUE;
+
+    public int minDiffInBST(TreeNode root) {
+        this.helper(root);
+        return this.ret;
+    }
+
+    private void helper(TreeNode root) {
+        if(root == null)
+            return;
+        helper(root.left);
+        this.update(root.val);
+        helper(root.right);
+    }
+
+    private void update(int newVal) {
+        if(this.prev != null && this.ret > newVal - this.prev)
+            this.ret = newVal - this.prev;
+        this.prev = newVal;
     }
 }
