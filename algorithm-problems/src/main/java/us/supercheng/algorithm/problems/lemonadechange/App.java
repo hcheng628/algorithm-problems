2bcebe09f1de4cacb43e70ebc076e41b52b6b20d
superhcheng <chenghongyu628@gmail.com>
2018-08-29 21:45:42 -0400

    add lemonade-change and different algorithm implementations

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/lemonadechange/App.java
@@ -0,0 +1,76 @@
+package us.supercheng.algorithm.problems.lemonadechange;
+
+import us.supercheng.algorithm.common.helper.PrintHelper;
+
+public class App {
+
+    public static void main(String[] args) {
+        int [] bills = {5,5,5,10,20};
+        PrintHelper.echoLn(new App().lemonadeChange(bills));
+        PrintHelper.echoLn(new App().lemonadeChange1(bills));
+    }
+
+    public boolean lemonadeChange(int[] bills) {
+        int[] wallet = new int [11];
+        for(int bill : bills) {
+            switch(bill) {
+                case 5:
+                    wallet[5]++;
+                    break;
+
+                case 10:
+                    if(wallet[5] == 0)
+                        return false;
+                    wallet[10]++;
+                    wallet[5]--;
+                    break;
+
+                default:
+                    if(wallet[10] == 0) {
+                        if(wallet[5] > 2)
+                            wallet[5] -=3;
+                        else
+                            return false;
+                    } else {
+                        wallet[10]--;
+                        if(wallet[5] > 0)
+                            wallet[5]--;
+                        else
+                            return false;
+                    }
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public boolean lemonadeChange1(int[] bills) {
+        int bill5=0, bill10=0;
+        for(int bill : bills) {
+            switch(bill) {
+                case 5:
+                    bill5++;
+                    break;
+                case 10:
+                    if(bill5 == 0)
+                        return false;
+                    bill10++;
+                    bill5--;
+                    break;
+                default:
+                    if(bill10 == 0) {
+                        if(bill5 < 3)
+                            return false;
+                        bill5 -=3;
+                    } else {
+                        if(bill5 < 1)
+                            return false;
+                        bill10--;
+                        bill5--;
+                    }
+                    break;
+            }
+        }
+        return true;
+    }
+}
--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/lettercasepermutation/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/lettercasepermutation/App.java
@@ -1,18 +1,25 @@
 package us.supercheng.algorithm.problems.lettercasepermutation;
 
 import us.supercheng.algorithm.common.helper.PrintHelper;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
         String S = "docker";
+
         for(String each : new App().letterCasePermutation(S))
-            PrintHelper.echoLn(each);
+            PrintHelper.echo(each + " ");
+
+        PrintHelper.echoLn("");
+
+        for(String each : new App().letterCasePermutation1(S))
+            PrintHelper.echo(each + " ");
+
+        PrintHelper.echoLn("");
+
+        for(String each : new App().letterCasePermutation2(S))
+            PrintHelper.echo(each + " ");
     }
 
     public List<String> letterCasePermutation(String S) {
@@ -41,5 +48,47 @@ public class App {
         if(!set.contains(sUpper))
             set.add(sUpper);
         helper(sUpper,index+1, set);
+    }
+
+    public List<String> letterCasePermutation1(String S) {
+        List<String> ret  = new ArrayList<>();
+        this.helper1(ret, S, 0);
+        return ret;
+    }
+
+    private void helper1(List<String> list, String s, int curr) {
+        if(curr == s.length()) {
+            list.add(s);
+            return;
+        }
+
+        char [] chars = s.toCharArray();
+        if(Character.isLetter(chars[curr])) {
+            chars[curr] = Character.toLowerCase(chars[curr]);
+            this.helper1(list, String.valueOf(chars), curr+1);
+            chars[curr] = Character.toUpperCase(chars[curr]);
+            this.helper1(list, String.valueOf(chars), curr+1);
+        } else
+            this.helper1(list, s, curr+1);
+    }
+
+    public List<String> letterCasePermutation2(String S) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add(S);
+        for (int i=0;i<S.length();i++) {
+            if(Character.isLetter(S.charAt(i))) {
+                int size = queue.size();
+                for(int j=0;j<size;j++) {
+                    char[] eachChars = queue.poll().toCharArray();
+                    queue.add(String.valueOf(eachChars));
+                    if(eachChars[i] < 97)
+                        eachChars[i] = Character.toLowerCase(eachChars[i]);
+                    else
+                        eachChars[i] = Character.toUpperCase(eachChars[i]);
+                    queue.add(String.valueOf(eachChars));
+                }
+            }
+        }
+        return (List)queue;
     }
 }
