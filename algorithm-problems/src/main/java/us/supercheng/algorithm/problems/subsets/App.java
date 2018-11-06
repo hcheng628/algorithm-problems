8b72ba19ad2a22fb82819b44a7c979cad411e65e
Hongyu Cheng <hcheng@cpex.com>
2018-11-06 18:08:36 -0500

    add subsets

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/subsets/App.java
@@ -0,0 +1,31 @@
+package us.supercheng.algorithm.problems.subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if(nums == null)
+            return ret;
+        ret.add(new ArrayList());
+        if(nums.length > 0) {
+            Arrays.sort(nums);
+            this.helper(nums, ret, new ArrayList<>(), 0);
+        }
+        return ret;
+    }
+
+    private void helper(int[] nums, List<List<Integer>> ret, List<Integer> list, int index) {
+        if(index > nums.length - 1)
+            return;
+        for(int i=index;i<nums.length;i++)
+            if(list.size() == 0 || list.get(list.size()-1) < nums[i]) {
+                list.add(nums[i]);
+                ret.add(new ArrayList<>(list));
+                this.helper(nums, ret, list, index+1);
+                list.remove(list.size()-1);
+            }
+    }
+}
