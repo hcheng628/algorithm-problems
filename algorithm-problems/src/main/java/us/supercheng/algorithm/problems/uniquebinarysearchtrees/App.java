cc853a6de8de9552724a1622acf9e466dada6ef8
superhcheng <chenghongyu628@gmail.com>
2018-11-24 16:24:28 -0500

    add unique-binary-search-trees DP approach

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/uniquebinarysearchtrees/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/uniquebinarysearchtrees/App.java
@@ -1,7 +1,7 @@
 package us.supercheng.algorithm.problems.uniquebinarysearchtrees;
 
 public class App {
-    public int numTrees(int n) {
+    public int numTreesMathDec(int n) {
         double top = 1.0, count = n;
 
         for(int i=n*2;i>=n+2;i--) {
@@ -12,5 +12,15 @@ public class App {
 
         for(;count >= 2;top /=count--);
         return (int) top;
+    }
+
+    public int numTrees(int n) {
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+            for(int j=0;j<i;j++)
+                dp[i] += dp[j] * dp[(i-j-1)];
+        return dp[n];
     }
 }
