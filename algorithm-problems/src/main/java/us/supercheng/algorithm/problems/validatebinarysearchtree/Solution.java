f72c44426a3d9831e5b0f90cd5231bf6f50f32b8
superhcheng <chenghongyu628@gmail.com>
2018-11-25 18:58:06 -0500

    add validate-binary-search-tree

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validatebinarysearchtree/Solution.java
@@ -0,0 +1,22 @@
+package us.supercheng.algorithm.problems.validatebinarysearchtree;
+
+import us.supercheng.algorithm.common.entity.TreeNode;`
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return root == null || this.helper(root, new ArrayList());
+    }
+
+    private boolean helper(TreeNode root, List<Integer> list) {
+        if(root.left != null && (root.left.val >= root.val || !this.helper(root.left, list)))
+            return false;
+
+        list.add(root.val);
+
+        return !(list.size() > 1 && list.get(list.size()-1) <= list.get(list.size()-2)) &&
+                !(root.right != null && (root.right.val <= root.val || !this.helper(root.right, list)));
+    }
+}
