412651e9ce8da930cc4337ff77b1e7e0c9214a8f
superhcheng <chenghongyu628@gmail.com>
2018-12-02 11:57:26 -0500

    add populating-next-right-pointers-in-each-node

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/common/entity/TreeLinkNode.java
@@ -0,0 +1,10 @@
+package us.supercheng.algorithm.common.entity;
+
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+
+    public TreeLinkNode(int x) {
+        val = x;
+    }
+}
