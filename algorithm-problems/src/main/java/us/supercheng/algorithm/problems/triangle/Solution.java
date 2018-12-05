59eb7b96601093616060cc459c4a65221f233190
superhcheng <chenghongyu628@gmail.com>
2018-12-05 00:02:18 -0500

    add triangle

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/triangle/Solution.java
@@ -0,0 +1,30 @@
+package us.supercheng.algorithm.problems.triangle;
+
+import java.util.List;
+
+class Solution {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+            return 0;
+        else if(triangle.size() == 1)
+            return triangle.get(0).get(0);
+
+        Integer ret = null, sizeIndex = triangle.size()-1;
+
+        for(int level=1;level<=sizeIndex;level++)
+            for(int index=0;index<triangle.get(level).size();index++) {
+                int curVal = triangle.get(level).get(index);
+                if(index <= triangle.get(level-1).size()-1 && index -1 >= 0)
+                    triangle.get(level).set(index, Math.min(curVal+triangle.get(level-1).get(index), curVal+triangle.get(level-1).get(index-1)));
+                else if(index > triangle.get(level-1).size()-1 && index -1 >=0)
+                    triangle.get(level).set(index, curVal + triangle.get(level-1).get(index-1));
+                else
+                    triangle.get(level).set(index, curVal + triangle.get(level-1).get(index));
+
+                if(level == sizeIndex)
+                    ret = ret == null ? triangle.get(level).get(index) : Math.min(ret, triangle.get(level).get(index));
+            }
+        return ret;
+    }
+}
