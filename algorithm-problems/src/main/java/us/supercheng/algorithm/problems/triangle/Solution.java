8944e05f999a201e6d708106e9e5f4c436599f74
superhcheng <chenghongyu628@gmail.com>
2018-12-06 23:51:00 -0500

    add sum-root-to-leaf-numbers and different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/triangle/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/triangle/Solution.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 class Solution {
 
-    public int minimumTotal(List<List<Integer>> triangle) {
+    public int minimumTotalSlow(List<List<Integer>> triangle) {
         if(triangle == null || triangle.size() == 0)
             return 0;
         else if(triangle.size() == 1)
@@ -26,5 +26,13 @@ class Solution {
                     ret = ret == null ? triangle.get(level).get(index) : Math.min(ret, triangle.get(level).get(index));
             }
         return ret;
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int [] dp = new int [triangle.size()+1];
+        for(int i=triangle.size()-1;i>-1;i--)
+            for(int j=0;j<triangle.get(i).size();j++)
+                dp[j] =  triangle.get(i).get(j) + Math.min(dp[j], dp[j+1]);
+        return dp[0];
     }
 }
