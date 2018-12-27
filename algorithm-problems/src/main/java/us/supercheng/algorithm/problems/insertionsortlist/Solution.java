561fc45b4baf6ea58cc4afc64faf37fb84a4a421
superhcheng <chenghongyu628@gmail.com>
2018-12-26 21:37:35 -0500

    add insertionsortlist

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/insertionsortlist/Solution.java
@@ -0,0 +1,46 @@
+package us.supercheng.algorithm.problems.insertionsortlist;
+
+import us.supercheng.algorithm.common.entity.ListNode;
+
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode curr = head,
+                ret = null;
+
+        while(curr != null) {
+            ListNode popNode = curr;
+            curr = curr.next;
+            ret = this.helper(popNode, ret);
+        }
+
+        return ret;
+    }
+
+    private ListNode helper(ListNode node, ListNode ret) {
+        if(ret == null) {
+            ret = node;
+            node.next = null;
+            return ret;
+        }
+
+        ListNode dummy = ret,
+                prev = null,
+                backup = ret;
+
+
+        for(;dummy != null;prev = dummy,dummy = dummy.next)
+            if(node.val <= dummy.val) {
+                if(prev == null) {
+                    node.next = backup;
+                    return node;
+                }
+                prev.next = node;
+                node.next = dummy;
+                return backup;
+            }
+
+        prev.next = node;
+        node.next = null;
+        return backup;
+    }
+}
