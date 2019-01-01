bd837f9287ecc856835d6a6d686e570c4f30e66f
superhcheng <chenghongyu628@gmail.com>
2019-01-01 11:08:30 -0500

    add find-minimumin-rotated-sorted-array

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/findminimuminrotatedsortedarray/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/findminimuminrotatedsortedarray/Solution.java
@@ -25,20 +25,15 @@ class Solution {
 
         while(left < right) {
             int mid = left + (right - left) / 2;
-
             if(nums[mid] > nums[mid+1])
                 return nums[mid+1];
-
             if(nums[mid] < nums[mid-1])
                 return nums[mid];
-
             if(nums[0] < nums[mid])
                 left = mid + 1;
             else
                 right = mid - 1;
-
         }
-
         return nums[0];
     }
 }
