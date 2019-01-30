2c7dd3ce55fab2b07d51208ee3ce51a60134c6e9
superhcheng <chenghongyu628@gmail.com>
2019-01-29 23:04:11 -0500

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximalsquare/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximalsquare/Solution.java
@@ -1,6 +1,7 @@
 package us.supercheng.algorithm.problems.maximalsquare;
 
 class Solution {
+
     public int maximalSquare(char[][] matrix) {
         int ret = 0;
         for (int row=0;row<matrix.length;row++)
@@ -32,6 +33,33 @@ class Solution {
                         ret = res;
                 }
             }
+        return ret * ret;
+    }
+
+    public int maximalSquareDP(char[][] matrix) {
+        if(matrix == null || matrix.length == 0)
+            return 0;
+        int ret = 0;
+        int [][] dp = new int [matrix.length][matrix[0].length];
+
+        for (int i=0;i<matrix.length;i++) {
+            dp[i][0] = matrix[i][0] - '0';
+            ret = Math.max(ret, dp[i][0]);
+        }
+
+        for (int i=0;i<matrix[0].length;i++) {
+            dp[0][i] = matrix[0][i] - '0';
+            ret = Math.max(ret, dp[0][i]);
+        }
+
+
+        for (int i=1;i<matrix.length;i++)
+            for (int j=1;j<matrix[i].length;j++)
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                    ret = Math.max(ret, dp[i][j]);
+                }
+
         return ret * ret;
     }
 }
