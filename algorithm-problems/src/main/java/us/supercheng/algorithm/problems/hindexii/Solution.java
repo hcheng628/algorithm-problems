0fbd43859f0c1626cb1d3a3fe99673ae61012294
superhcheng <chenghongyu628@gmail.com>
2019-02-11 23:44:16 -0500

    add h-index i and ii

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/hindexii/Solution.java
@@ -0,0 +1,19 @@
+package us.supercheng.algorithm.problems.hindexii;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0)
+            return 0;
+        int left=0, right=citations.length-1, len = citations.length;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left, count = len - mid;
+            if (citations[mid] == count)
+                return count;
+            else if (citations[mid] >= count)
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return len - left;
+    }
+}
