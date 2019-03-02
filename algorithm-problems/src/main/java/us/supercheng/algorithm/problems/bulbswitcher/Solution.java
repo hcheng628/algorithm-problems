733a3c49e2e2eefa24522f3699b02f49db3e19d4
superhcheng <chenghongyu628@gmail.com>
2019-03-02 08:48:59 -0500

    add bulb-switcher

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/bulbswitcher/Solution.java
@@ -0,0 +1,22 @@
+package us.supercheng.algorithm.problems.bulbswitcher;
+
+class Solution {
+    public int bulbSwitch(int n) {
+        if (n < 1)
+            return 0;
+        else if (n < 4)
+            return 1;
+
+        int ret = 0, capacity=3;
+        while (n>0) {
+            int currCapacity = capacity;
+            while(capacity > 0) {
+                capacity--;
+                n--;
+            }
+            capacity=currCapacity+2;
+            ret++;
+        }
+        return ret;
+    }
+}
