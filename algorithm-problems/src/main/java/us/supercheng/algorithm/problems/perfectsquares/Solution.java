a779e8ddd624b5f5f7cd528d10e4fc6988be2484
superhcheng <chenghongyu628@gmail.com>
2019-03-10 17:32:47 -0400

    add different-implementation

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/perfectsquares/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/perfectsquares/Solution.java
@@ -1,7 +1,10 @@
 package us.supercheng.algorithm.problems.perfectsquares;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
-    public int numSquares(int n) {
+    public int numSquaresDP(int n) {
         int [] dp = new int [n+1];
         for (int i=1;i<=n;i++) {
             int res = Integer.MAX_VALUE;
@@ -10,5 +13,29 @@ class Solution {
             dp[i] = res;
         }
         return dp[n];
+    }
+
+    public int numSquares(int n) {
+        Queue<int []> queue = new LinkedList<>();
+        boolean [] visited = new boolean [n];
+        int [] initPoint = {n, 0};
+        queue.add(initPoint);
+        while (!queue.isEmpty()) {
+            int [] eachFromTo = queue.poll();
+            for (int i=1;;i++) {
+                int res = eachFromTo[0]- i*i;
+                if (res == 0)
+                    return eachFromTo[1] + 1;
+                else if (res < 0)
+                    break;
+                if (!visited[res]) {
+                    int [] eachBFSPoint = {res, eachFromTo[1] + 1};
+                    queue.add(eachBFSPoint);
+                    visited[res] = true;
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid Input");
     }
 }
