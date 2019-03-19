bcf3096549b86544685d450166b8f8e1f9a00959
superhcheng <chenghongyu628@gmail.com>
2019-03-18 21:32:04 -0400

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/permutationsii/App.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/permutationsii/App.java
@@ -42,4 +42,33 @@ public class App {
             for(;i<nums.length-1 && nums[i] == nums[i+1];i++);
         }
     }
+
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (nums == null || nums.length == 0)
+            return ret;
+
+        Arrays.sort(nums);
+        this.dfs(ret, nums, 0, new ArrayList<>(), new boolean [nums.length]);
+        return ret;
+    }
+
+    private void dfs(List<List<Integer>> ret, int [] nums, int idx, List<Integer> list, boolean [] visited) {
+        if (list.size() == nums.length) {
+            ret.add(new ArrayList(list));
+            return;
+        }
+
+        for (int i=0;i<nums.length;i++)
+            if (!visited[i]) {
+                if (i > 0 && nums[i-1] == nums[i] && !visited[i-1])
+                    continue;
+
+                list.add(nums[i]);
+                visited[i] = true;
+                this.dfs(ret, nums, idx+1, list, visited);
+                list.remove(list.size()-1);
+                visited[i] = false;
+            }
+    }
 }
