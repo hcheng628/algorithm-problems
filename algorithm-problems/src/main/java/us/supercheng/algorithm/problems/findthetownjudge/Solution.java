a2e1953f4a72142660b7ba994f5e5f9dbf869cd1
superhcheng <chenghongyu628@gmail.com>
2019-03-22 21:36:44 -0400

    add find-the-town-judge

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/findthetownjudge/Solution.java
@@ -0,0 +1,18 @@
+package us.supercheng.algorithm.problems.findthetownjudge;
+
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int [] votes = new int [N+1],
+                receives = new int [votes.length];
+
+        for (int [] each : trust) {
+            votes[each[0]]++;
+            receives[each[1]]++;
+        }
+
+        for (int i=1;i<votes.length;i++)
+            if (receives[i] == N-1 && votes[i] == 0)
+                return i;
+        return -1;
+    }
+}
