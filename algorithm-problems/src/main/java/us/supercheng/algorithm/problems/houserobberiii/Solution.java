6fb1fb0ed4d7b2bfb044b156b08a3c69df66b42d
superhcheng <chenghongyu628@gmail.com>
2019-03-22 22:12:20 -0400

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/houserobberiii/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/houserobberiii/Solution.java
@@ -16,4 +16,21 @@ class Solution {
 
         return Math.max(val, this.rob(root.left) + this.rob(root.right));
     }
+
+    public int robDP(TreeNode root) {
+        int[] res = this.dfsDP(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] dfsDP(TreeNode root) {
+        if (root == null)
+            return new int [2];
+        int [] left = this.dfsDP(root.left);
+        int [] right = this.dfsDP(root.right);
+
+        int[] res = new int [2];
+        res[0] = root.val + left[1] + right[1];
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
+    }
 }
