c9f28b8723e790e4b07b899e56a0cc4cedee48cc
superhcheng <chenghongyu628@gmail.com>
2019-04-03 22:45:03 -0400

    add different implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/numberofislands/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/numberofislands/Solution.java
@@ -28,4 +28,43 @@ class Solution {
         if(j > 0 && grid[i][j-1] == '1')
             this.markSameIsland(grid, i, j-1);
     }
+
+
+    /* Different Implementation */
+
+    private final int[][] DIR = {{1,0},{-1,0},{0,1},{0,-1}};
+    private boolean[][] visited;
+
+    public int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+
+        this.visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
+
+        for (int i=0;i<grid.length;i++)
+            for (int j=0;j<grid[i].length;j++)
+                if (grid[i][j] == '1' && !this.visited[i][j]) {
+                    this.bfs(grid, i, j);
+                    count++;
+                }
+
+        return count;
+
+    }
+
+    private void bfs(char[][] grid, int x, int y) {
+        this.visited[x][y] = true;
+
+        for (int i=0;i<4;i++) {
+            int newX = DIR[i][0] + x,
+                    newY = DIR[i][1] + y;
+            if (this.isInArea(grid, newX, newY) && !this.visited[newX][newY] && grid[newX][newY] == '1')
+                this.bfs(grid, newX, newY);
+        }
+    }
+
+    private boolean isInArea(char[][] grid, int x, int y) {
+        return x > -1 && y > -1 && x < grid.length && y < grid[x].length;
+    }
 }
