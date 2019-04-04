2648b5bd54e97262d52b0a7c2005ecd327c7f926
superhcheng <chenghongyu628@gmail.com>
2019-04-03 21:49:57 -0400

    add linked-list-random-node

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/linkedlistrandomnode/Solution.java
@@ -0,0 +1,34 @@
+package us.supercheng.algorithm.problems.linkedlistrandomnode;
+
+import us.supercheng.algorithm.common.entity.ListNode;
+import java.util.Random;
+
+class Solution {
+
+    ListNode dummyHead;
+    int length;
+    Random random;
+
+    /** @param head The linked list's head.
+    Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.random = new Random();
+        dummyHead = new ListNode(13);
+        dummyHead.next = head;
+
+        for (ListNode curr = head;curr != null; curr = curr.next)
+            this.length++;
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+        if (this.length == 0)
+            throw new RuntimeException("Empty list nothing to return");
+
+        ListNode curr = this.dummyHead.next;
+        for (int i=0, randIdx = this.random.nextInt(length);i<randIdx;i++)
+            curr = curr.next;
+
+        return curr.val;
+    }
+}
