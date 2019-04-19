55eeef9a4f9ebab787d7b39ea82e8478f0d1f57b
superhcheng <chenghongyu628@gmail.com>
2019-04-18 23:06:48 -0400

    add different algorithm-implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/nonoverlappingintervals/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/nonoverlappingintervals/Solution.java
@@ -7,20 +7,31 @@ import java.util.Comparator;
 
 class Solution {
 
-    public int eraseOverlapIntervals(Interval[] intervals) {
-        if (intervals == null || intervals.length == 0)
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals == null || intervals.length < 2)
             return 0;
 
-        Arrays.sort(intervals, new Comparator<Interval>() {
+        Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
-            public int compare(Interval one, Interval theOther) {
-                if (one.start != theOther.start)
-                    return one.start - theOther.start;
-                return one.end - theOther.end;
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0])
+                    return o1[0] - o2[0];
+                return o1[1] - o2[1];
             }
         });
 
-        return -1;
+        int [] dp = new int [intervals.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+
+        for (int i=0;i<dp.length;i++) {
+            for (int j=0;j<i;j++)
+                if(intervals[j][1] <= intervals[i][0])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            max = Math.max(dp[i], max);
+        }
+
+        return intervals.length - max;
     }
 
 }
--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/test/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/test/Solution.java
@@ -10,46 +10,7 @@ class Solution {
         int [][] nums = {{1,1,0},{1,1,0},{0,0,1}};
 
         Solution solution = new Solution();
-        int res = solution.numSquares(12);
-
-    }
-
-    public int numSquares(int n) {
-
-        List<Integer> list = new ArrayList<>();
-        int i=1;
-        for (;true;i++) {
-            int res = i*i;
-            if (res > n) {
-                i--;
-                break;
-            } else if (res == n) {
-                return 1;
-            }
-
-            list.add(res);
-        }
-
-        //PrintHelper.echoLn("i === " + i);
-
-        List<Integer> ret = new ArrayList<>();
-
-        this.helper(list, ret, 0, n);
-        return 0;
-    }
-
-    private void helper(List<Integer> list, List<Integer> ret, int i, int res) {
-        if(res - list.get(i) < 0)
-            return;
-        else if (res - list.get(i) == 0) {
-            ret.add(list.get(i));
-            return;
-        }
-
-        for (int j=0;j<list.size();j++) {
-
-        }
-
+        //int res = solution.eraseOverlapIntervals(null);
 
     }
 }
