cc39a9f85dc6b4364f00931ce55bbd2389ebabb1
superhcheng <chenghongyu628@gmail.com>
2019-05-26 07:15:31 -0400

    add height-checker

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/heightchecker/Solution.java
@@ -0,0 +1,24 @@
+package us.supercheng.algorithm.problems.heightchecker;
+
+import java.util.Arrays;
+
+class Solution {
+
+    public int heightChecker(int[] heights) {
+        if (heights == null || heights.length < 2)
+            return 0;
+
+        int ret = 0,
+            len = heights.length;
+
+        int[] copyHeights = Arrays.copyOf(heights, len);
+
+        Arrays.sort(copyHeights);
+
+        for (int i=0;i<len;i++)
+            if (heights[i] != copyHeights[i])
+                ret++;
+
+        return ret;
+    }
+}
