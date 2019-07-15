18433f4f2efa4cb5aec5b831204e5289124c5a05
Hongyu Cheng <chenghongyu628@gmail.com>
2019-07-14 21:53:58 -0400

    add distribute-candies-to-people

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/distributecandiestopeople/Solution.java
@@ -0,0 +1,21 @@
+package us.supercheng.algorithm.problems.distributecandiestopeople;
+
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        if (num_people < 1)
+            return null;
+        else if (candies < 1)
+            return new int[num_people];
+
+        int[] ret = new int[num_people];
+
+        for(int n=1, idx = 0;candies > 0; idx++,candies -=n++)
+            if (n > candies) {
+                ret[idx % num_people] += candies;
+                break;
+            } else
+                ret[idx % num_people] += n;
+
+        return ret;
+    }
+}
