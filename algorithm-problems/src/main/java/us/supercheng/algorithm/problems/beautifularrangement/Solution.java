bafc22a9387f5e4aca2e6f97d61af772c856ebe0
Hongyu Cheng <chenghongyu628@gmail.com>
2019-08-07 23:19:21 -0400

    add different algorithm implementation

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/beautifularrangement/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/beautifularrangement/Solution.java
@@ -1,36 +1,27 @@
 package us.supercheng.algorithm.problems.beautifularrangement;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
 
     int count;
 
     public int countArrangement(int N) {
         boolean[] arr = new boolean[N+1];
-        List<Integer> list = new ArrayList<>();
         this.count = 0;
-        this.helper(arr, 1, list);
+        this.helper(arr, 1);
         return this.count;
     }
 
-    private void helper(boolean[] arr, int idx, List<Integer> list) {
-        if (list.size() == arr.length-1) {
+    private void helper(boolean[] arr, int idx) {
+        if (idx == arr.length) {
             this.count++;
             return;
         }
 
-        for (int i=1;i<arr.length;i++) {
-            if (arr[i])
-                continue;
-            if (idx % i == 0 || i % idx == 0) {
-                list.add(idx);
+        for (int i=1;i<arr.length;i++)
+            if (!arr[i] && (idx % i == 0 || i % idx == 0)) {
                 arr[i] = true;
-                this.helper(arr, idx+1, list);
+                this.helper(arr, idx+1);
                 arr[i] = false;
-                list.remove(list.size()-1);
             }
-        }
     }
 }
