13d0699c510554d04d3b6e22eb5f36879045c767
Hongyu Cheng <chenghongyu628@gmail.com>
2019-08-26 21:47:49 -0400

    add different implementation + valid-square

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/numberofequivalentdominopairs/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/numberofequivalentdominopairs/Solution.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+
     public int numEquivDominoPairs(int[][] dominoes) {
         int ret = 0;
         Map<String, Integer> map = new HashMap<>();
-        
+
         for (int[] row : dominoes) {
             String s = null;
             if (row[0] > row[1])
@@ -21,6 +22,19 @@ class Solution {
             int val = map.get(key);
             if (val-- > 1)
                 ret += (val+1)*val/2;
+        }
+
+        return ret;
+    }
+
+    public int numEquivDominoPairs2(int[][] dominoes) {
+        int ret = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] dom : dominoes) {
+            int res = Math.max(dom[0], dom[1]) + Math.min(dom[0], dom[1]) * 10,
+                occurs = map.getOrDefault(res, 0);
+            ret += occurs;
+            map.put(res, occurs+1);
         }
 
         return ret;
---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validsquare/Solution.java
@@ -0,0 +1,45 @@
+package us.supercheng.algorithm.problems.validsquare;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+
+        int[][] arr = new int[][]{p1, p2, p3, p4};
+        int[] topLeft, topRight, botLeft, botRight;
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[1] - b[1];
+            }
+        });
+
+        if (arr[3][0] < arr[2][0]) {
+            topLeft = arr[3];
+            topRight = arr[2];
+        } else {
+            topLeft = arr[2];
+            topRight = arr[3];
+        }
+
+        if (arr[1][0] < arr[0][0]) {
+            botLeft = arr[1];
+            botRight = arr[0];
+        } else {
+            botLeft = arr[0];
+            botRight = arr[1];
+        }
+
+        int topLine = Math.abs(topLeft[0] - topRight[0]),
+            botLine = Math.abs(botLeft[0] - botRight[0]),
+            leftLine = Math.abs(topLeft[1] - botLeft[1]),
+            rightLine = Math.abs(topRight[1] - botRight[1]);
+
+        return topLine == botLine && botLine == leftLine && leftLine == rightLine &&
+                Math.pow(topLeft[0] - botRight[0], 2) + Math.pow(topLeft[1] - botRight[1], 2) ==
+                        Math.pow(topRight[0] - botLeft[0], 2) + Math.pow(topRight[1] - botLeft[1], 2) &&
+                topLine * botLine > 0;
+    }
+}
