294d0b0ad65192c5afe0c741228ed3ba65864357
Hongyu Cheng <chenghongyu628@gmail.com>
2019-09-13 23:50:53 -0400

    enhancement

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validatebinarysearchtree/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validatebinarysearchtree/Solution.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 class Solution {
 
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST1(TreeNode root) {
         return root == null || this.helper(root, new ArrayList());
     }
 
@@ -18,5 +18,18 @@ class Solution {
 
         return !(list.size() > 1 && list.get(list.size()-1) <= list.get(list.size()-2)) &&
                 !(root.right != null && (root.right.val <= root.val || !this.helper(root.right, list)));
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return this.helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean helper(TreeNode root, long MIN, long MAX) {
+        if (root == null)
+            return true;
+        else if (root.val < MIN || root.val > MAX)
+            return false;
+        return this.helper(root.left, MIN,  ((long) root.val) - 1) &&
+                this.helper(root.right, ((long) root.val) + 1, MAX);
     }
 }
