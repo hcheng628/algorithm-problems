85384b7a9a6383c5285d27602f7e40baae6a25e3
Hongyu Cheng <chenghongyu628@gmail.com>
2019-09-29 15:31:00 -0400

    add different implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/palindromicsubstrings/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/palindromicsubstrings/Solution.java
@@ -53,6 +53,50 @@ class Solution {
         }
     }
 
+    public int countSubstringsDP(String s) {
+        char[] chars = s.toCharArray();
+        int len = chars.length,
+                ret = len;
+        boolean[][] dp = new boolean[len][len];
+
+        for (int i=0;i<len;i++) {   // init dp
+            dp[i][i] = true;
+            if (i+1<len && chars[i] == chars[i+1]) {
+                dp[i][i+1] = true;
+                ret++;
+            }
+        }
+
+        // first == last and inner inclusive dp is true
+        for (int currLen=3;currLen<=len;currLen++)
+            for (int i=0;i<=len-currLen;i++) {
+                dp[i][i+currLen-1] = dp[i+1][i+currLen-2] && chars[i] == chars[i+currLen-1];
+                if (dp[i][i+currLen-1])
+                    ret++;
+            }
+
+        return ret;
+    }
+
+    public int countSubstringsExpandSearch(String s) {
+        char[] chars = s.toCharArray();
+        int len = chars.length,
+                ret = 0;
+
+        for (int i=0;i<len;i++)
+            ret += this.countPa(chars, i, i) + this.countPa(chars, i, i+1);
+
+        return ret;
+    }
+
+    private int countPa(char[] chars, int left, int right) {
+        int ret = 0;
+        while (left>=0 && right<chars.length && chars[left--] == chars[right++])
+            ret++;
+
+        return ret;
+    }
+
     private void echo(Object o) {
         System.out.println(o);
     }
