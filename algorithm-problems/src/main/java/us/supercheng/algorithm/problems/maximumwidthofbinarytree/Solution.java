82e887705eadeb83d0f4c6d3cb82f1de24b40786
Hongyu Cheng <chenghongyu628@gmail.com>
2019-10-25 22:35:00 -0400

    add different algorithm implementation

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximumwidthofbinarytree/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximumwidthofbinarytree/Solution.java
@@ -2,7 +2,9 @@ package us.supercheng.algorithm.problems.maximumwidthofbinarytree;
 
 import us.supercheng.algorithm.common.entity.TreeNode;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 class Solution {
 
@@ -26,5 +28,45 @@ class Solution {
 
         this.dfs(root.left, level+1, idx*2 + 1, leftMap, rightMap);
         this.dfs(root.right, level+1, idx*2 + 2, leftMap, rightMap);
+    }
+
+    public int widthOfBinaryTreeBFS(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer>  list = new LinkedList<>();
+        int ret = 1;
+        queue.add(root);
+        list.add(1);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size(),
+                    left = 0,
+                    right = 0;
+
+            for (int i=0;i<size;i++) {
+                TreeNode node = queue.remove();
+                int idx = list.remove();
+
+                if (i == 0)
+                    left = idx;
+
+                if (i == size-1)
+                    right = idx;
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                    list.add(idx*2);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                    list.add(idx*2+1);
+                }
+            }
+            ret = Math.max(right - left + 1, ret);
+        }
+        return ret;
     }
 }
