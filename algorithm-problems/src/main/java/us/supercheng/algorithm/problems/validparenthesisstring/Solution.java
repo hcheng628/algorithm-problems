f99d865074483da27cede9e8070d214c3779cd41
Hongyu Cheng <chenghongyu628@gmail.com>
2019-11-10 16:40:53 -0500

    valid-parenthesis-string

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validparenthesisstring/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/validparenthesisstring/Solution.java
@@ -3,12 +3,48 @@ package us.supercheng.algorithm.problems.validparenthesisstring;
 import java.util.Stack;
 
 class Solution {
-    public boolean checkValidString(String s) {
+
+    private Boolean[][] mat;
+
+    public boolean checkValidStringMemo(String s) {
+        if (s == null || s.length() < 1)
+            return true;
+
+        int len = s.length();
+        this.mat = new Boolean[len][len];
+        return this.isValid(s, 0, len-1);
+    }
+
+    private boolean isValid(String s, int from, int to) {
+        if (from > to)
+            return true;
+
+        if (this.mat[from][to] != null)
+            return this.mat[from][to];
+
+        if (from == to && s.charAt(from) == '*')
+            return this.mat[from][to] = true;
+
+        char left = s.charAt(from),
+                right = s.charAt(to);
+        if ((left == '*' || left == '(') &&
+                (right == '*' || right == ')') &&
+                this.isValid(s, from+1, to-1))
+            return this.mat[from][to] = true;
+
+        for (int i=from;i<to;i++)
+            if (this.isValid(s, from, i) && this.isValid(s, i+1, to))
+                return this.mat[from][to] = true;
+
+        return this.mat[from][to] = false;
+    }
+
+
+    public boolean checkValidStringLTE(String s) {
         if (s == null || s.length() < 1)
             return true;
         return this.helper(s, 0, s.length());
     }
-
 
     public boolean helper(String s, int idx, int len) {
         if (idx == len)
