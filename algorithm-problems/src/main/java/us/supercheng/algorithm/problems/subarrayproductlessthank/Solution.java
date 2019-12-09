363c92b91b5d1bf25a350fcfa6a54c30e6d3d93b
Hongyu Cheng <chenghongyu628@gmail.com>
2019-12-08 21:04:34 -0800

    subarray-product-less-than-k

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/subarrayproductlessthank/Solution.java
@@ -0,0 +1,22 @@
+package us.supercheng.algorithm.problems.subarrayproductlessthank;
+
+class Solution {
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k < 2)
+            return 0;
+
+        int ret = 0,
+                left = 0;
+        double curr = 1;
+
+        for (int right=0, len = nums.length;right<len;right++) {
+            curr *= nums[right];
+            while(left < len && curr >= k)
+                curr /= nums[left++];
+            ret += right - left + 1;
+        }
+
+        return ret;
+    }
+}
