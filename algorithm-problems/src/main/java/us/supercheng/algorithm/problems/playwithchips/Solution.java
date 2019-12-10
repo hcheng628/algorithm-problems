a1a7e663c6b0a77870d46c93e246982a08013fcf
Hongyu Cheng <chenghongyu628@gmail.com>
2019-12-09 23:25:52 -0500

    play-with-chips

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/playwithchips/Solution.java
@@ -0,0 +1,16 @@
+package us.supercheng.algorithm.problems.playwithchips;
+
+class Solution {
+
+    public int minCostToMoveChips(int[] chips) {
+        int oddMove = 0,
+            evenMove = 0;
+
+        for (int chip : chips)
+            if (chip % 2 == 0)
+                evenMove++;
+            else
+                oddMove++;
+        return Math.min(oddMove, evenMove);
+    }
+}
