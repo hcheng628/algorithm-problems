b0fe9e65613eb033a549e642a77ce9e13976225c
Hongyu Cheng <chenghongyu628@gmail.com>
2019-12-23 23:44:33 -0500

    check-if-it-is-a-straight-line

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/checkifitisastraightline/Solution.java
@@ -0,0 +1,21 @@
+package us.supercheng.algorithm.problems.checkifitisastraightline;
+
+public class Solution {
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates == null || coordinates.length < 2)
+            return true;
+
+        int deltaX = coordinates[0][0] - coordinates[1][0],
+                deltaY = coordinates[0][1] - coordinates[1][1];
+
+        for (int i=2, len=coordinates.length;i<len;i++) {
+            int deltaCurrX = coordinates[i][0] - coordinates[i-1][0],
+                    deltaCurrY = coordinates[i][1] - coordinates[i-1][1];
+            if (deltaX * deltaCurrY != deltaY * deltaCurrX)
+                return false;
+        }
+
+        return true;
+    }
+}
