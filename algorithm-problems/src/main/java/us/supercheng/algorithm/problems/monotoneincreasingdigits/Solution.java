e42c2218963ee5cdd1ed48f7f2d027d5f0c58b53
SuperCheng <chenghongyu628@gmail.com>
2019-12-27 00:02:08 -0500

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/monotoneincreasingdigits/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/monotoneincreasingdigits/Solution.java
@@ -35,4 +35,55 @@ public class Solution {
 
         return Integer.parseInt(sb.toString());
     }
+
+    public int monotoneIncreasingDigitsGreedy(int N) {
+        if (N < 10)
+            return N;
+
+        String S = String.valueOf(N),
+               ans = "";
+        int len = S.length();
+        StringBuilder sb = new StringBuilder();
+
+        buildEachChar:
+        for (int i=0;i<len;i++) {
+            for (char c = '1';c <= '9'; c++) {
+                if (S.compareTo(ans + this.buildStr(c, len-i, sb)) < 0) {
+                    ans += (char) (c-1);
+                    continue buildEachChar;
+                }
+            }
+            ans += '9';
+        }
+
+        return Integer.parseInt(ans);
+    }
+
+    private String buildStr(char c, int len, StringBuilder sb) {
+        if (sb.length() != 0)
+            sb.setLength(0);
+        for (int i=0;i<len;i++)
+            sb.append(c);
+        return sb.toString();
+    }
+
+    public int monotoneIncreasingDigitsOpt(int N) {
+        if (N < 10)
+            return N;
+
+        char[] chars = String.valueOf(N).toCharArray();
+        int idx = 1,
+            len = chars.length;
+
+        while (idx < len && chars[idx-1] <= chars[idx])
+            idx++;
+
+        while (idx < len && idx > 0 && chars[idx-1] > chars[idx])
+            chars[--idx]--;
+
+        for (int i=idx+1;i<len;i++)
+            chars[i] = '9';
+
+        return Integer.parseInt(new String(chars));
+    }
 }
