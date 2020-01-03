f73365d31302a14c7f2502bafd2d35d68c4d4d8b
chenghongyu628@gmail.com <chenghongyu628@gmail.com>
2020-01-02 22:42:16 -0500

    reach-a-number

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/reachanumber/Solution.java
@@ -0,0 +1,18 @@
+package us.supercheng.algorithm.problems.reachanumber;
+
+public class Solution {
+
+    public int reachNumber(int target) {
+        if (target < 0)
+            target = -target;
+
+        int n = (int) Math.ceil((-1.0 + Math.sqrt(1 + 8.0 * target)) / 2),
+            sum = (1+n)*n/2,
+            res = sum - target;
+
+        if (sum == target || res % 2 == 0)
+            return n;
+        else
+            return n % 2 == 0 ? n + 1 : n + 2;
+    }
+}
