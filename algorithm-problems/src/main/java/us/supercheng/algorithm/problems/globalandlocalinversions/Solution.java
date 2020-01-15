fe2c6fb4c94c1daa2fc4d7fb5c86f1f9d1f001a8
Hongyu Cheng <chenghongyu628@gmail.com>
2020-01-14 22:50:54 -0500

    add different implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/globalandlocalinversions/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/globalandlocalinversions/Solution.java
@@ -26,4 +26,50 @@ public class Solution {
                 count++;
         return count;
     }
+
+    public boolean isIdealPermutationMS(int[] A) {
+        if (A == null || A.length < 1)
+            return true;
+
+        int len = A.length,
+            loc = 0;
+
+        for (int i=0;i<len-1;i++)
+            if (A[i] > A[i+1])
+                loc++;
+
+        return this.mergeSort(A, 0, len) == loc;
+    }
+
+    private int mergeSort(int[] arr, int left, int right) {
+        if (left + 1 >= right)
+            return 0;
+
+        int mid = (right - left) / 2 + left,
+            res = this.mergeSort(arr, left, mid) + this.mergeSort(arr, mid, right),
+            l = left,
+            r = mid,
+            idx = 0;
+
+        int[] copy = new int[right-left];
+
+        while (l<mid && r < right)
+            if (arr[l] < arr[r])
+                copy[idx++] = arr[l++];
+            else {
+                res += mid - l;
+                copy[idx++] = arr[r++];
+            }
+
+        while(l<mid)
+            copy[idx++] = arr[l++];
+
+        while(r<right)
+            copy[idx++] = arr[r++];
+
+        for (int i=left,j=0;i<right;i++,j++)
+            arr[i] = copy[j];
+
+        return res;
+    }
 }
