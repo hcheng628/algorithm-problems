1bbeeffd411d3f7fb8d5aef23ce64011b74162e1
Hongyu Cheng <chenghongyu628@gmail.com>
2020-01-26 23:47:35 -0500

    element-appearing-more-than-25-in-sorted-array

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/elementappearingmorethan25insortedarray/Solution.java
@@ -0,0 +1,24 @@
+package us.supercheng.algorithm.problems.elementappearingmorethan25insortedarray;
+
+public class Solution {
+
+    public int findSpecialInteger(int[] arr) {
+        if (arr.length == 1)
+            return arr[0];
+
+        int count = (int) Math.ceil(arr.length / 4.0);
+
+        for (int slow=0,fast=0,len = -1,length=arr.length;fast < length;) {
+            if (arr[slow] == arr[fast])
+                len = ++fast - slow;
+            else {
+                len = fast - slow;
+                slow = fast;
+            }
+            if (len > count)
+                return arr[slow];
+        }
+
+        return -1;
+    }
+}
