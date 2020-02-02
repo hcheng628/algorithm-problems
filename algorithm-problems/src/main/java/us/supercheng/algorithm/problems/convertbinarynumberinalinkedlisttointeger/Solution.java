6da3188762bc8dfcc31a9a621c8c9501798c9ec8
Hongyu Cheng <chenghongyu628@gmail.com>
2020-02-01 23:47:55 -0500

    convert-binary-number-in-a-linked-list-to-integer

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/convertbinarynumberinalinkedlisttointeger/Solution.java
@@ -0,0 +1,19 @@
+package us.supercheng.algorithm.problems.convertbinarynumberinalinkedlisttointeger;
+
+import us.supercheng.algorithm.common.entity.ListNode;
+
+public class Solution {
+
+    public int getDecimalValue(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+
+        for (;head != null;head = head.next)
+            sb.append(head.val);
+
+        int ret = 0;
+        for (int i=sb.length()-1, pow = 0;i>-1;i--)
+            ret += (sb.charAt(i) == '0' ? 0 : 1) * (int) Math.pow(2, pow++);
+
+        return ret;
+    }
+}
