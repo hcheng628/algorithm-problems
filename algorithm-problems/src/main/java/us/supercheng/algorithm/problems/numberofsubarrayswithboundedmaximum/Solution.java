b1d2b8027d0eaf37a9b5cf10d0e4412be8a45ba7
Hongyu Cheng <chenghongyu628@gmail.com>
2020-02-09 20:55:21 -0500

    number-of-subarrays-with-bounded-maximum

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/numberofsubarrayswithboundedmaximum/Solution.java
@@ -0,0 +1,24 @@
+package us.supercheng.algorithm.problems.numberofsubarrayswithboundedmaximum;
+
+public class Solution {
+
+    public int numSubarrayBoundedMax(int[] A, int L, int R) {
+        int len = A.length,
+            ret = 0;
+
+        for (int i=0;i<len;i++)
+            if (A[i] <= R) {
+                int max = A[i];
+                for (int j=i;j<len;j++) {
+                    if (A[j] > R)
+                        break;
+
+                    max = Math.max(max, A[j]);
+                    if (max >= L)
+                        ret++;
+                }
+            }
+
+        return ret;
+    }
+}
