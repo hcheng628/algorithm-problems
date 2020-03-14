7a8b1ac5c06263b3002dd8ed62e1f460a1f18fb5
Hongyu Cheng <chenghongyu628@gmail.com>
2020-03-14 15:33:56 -0400

    different implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/convertanumbertohexadecimal/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/convertanumbertohexadecimal/Solution.java
@@ -5,6 +5,22 @@ import java.util.Map;
 
 class Solution {
 
+    public String toHex_BIT(int num) {
+        if (num == 0)
+            return "0";
+
+        char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0;i<8 && num != 0;i++) {
+            int key = num & 0b1111;
+            sb.insert(0, map[key]);
+            num = num >> 4;
+        }
+
+        return sb.toString();
+    }
+
     public String toHex(int num) {
         if (num == 0)
             return "0";
