792ea66bab2671ade992fae53b00f8b35c0c03a6
Hongyu Cheng <chenghongyu628@gmail.com>
2020-04-12 22:22:23 -0400

    maximum-69-number

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/maximum69number/Solution.java
@@ -0,0 +1,17 @@
+package us.supercheng.algorithm.problems.maximum69number;
+
+class Solution {
+    public int maximum69Number (int num) {
+        int ret = num;
+        char[] chars = Integer.toString(num).toCharArray();
+
+        for (int i=0, len=chars.length; i<len; i++)
+            if (chars[i] == '6') {
+                chars[i] = '9';
+                ret = Math.max(ret, Integer.parseInt(new String(chars)));
+                chars[i] = '6';
+            }
+
+        return ret;
+    }
+}
