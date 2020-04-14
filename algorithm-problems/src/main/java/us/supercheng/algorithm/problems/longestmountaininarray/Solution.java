f9dd054474716af0694604de87d54482779207da
Hongyu Cheng <chenghongyu628@gmail.com>
2020-04-14 14:52:22 -0400

    add O(n) solution

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/longestmountaininarray/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/longestmountaininarray/Solution.java
@@ -38,4 +38,47 @@ public class Solution {
 
         return ret;
     }
+
+    public int longestMountainBigO_N(int[] A) {
+        if (A == null || A.length < 3)
+            return 0;
+
+        int left = 0,
+                ret = 0;
+        boolean hasInc = A[0] < A[1],
+                hasDec = false;
+
+        for (int i=1; i<A.length; i++)
+            if (A[i-1] < A[i]) {
+                if (hasInc && hasDec) {
+                    hasDec = false;
+                    left = i-1;
+                } else if (!hasInc && !hasDec)
+                    hasInc = true;
+                else if (!hasInc) {
+                    hasInc = true;
+                    hasDec = false;
+                    left = i;
+                }
+            } else if (A[i-1] == A[i]) {
+                hasInc = false;
+                hasDec = false;
+                left = i;
+            } else {
+                if (hasInc && hasDec) {
+                    if (i - left + 1 >= 3)
+                        ret = Math.max(ret, i - left + 1);
+                } else if (hasInc) {
+                    if (i - left + 1 >= 3)
+                        ret = Math.max(ret, i - left + 1);
+                    hasDec = true;
+                } else {
+                    if (hasDec)
+                        hasDec = false;
+                    left = i;
+                }
+            }
+
+        return ret;
+    }
 }
