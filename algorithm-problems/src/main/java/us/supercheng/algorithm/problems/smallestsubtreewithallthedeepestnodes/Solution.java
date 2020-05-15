c94ee7ffbd3db1fee51eb8a1a33b1356311e8977
Hongyu Cheng <chenghongyu628@gmail.com>
2020-05-14 22:05:30 -0400

    add different implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/smallestsubtreewithallthedeepestnodes/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/smallestsubtreewithallthedeepestnodes/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
 
     private Info dfs(TreeNode root, int level) {
         if (root == null)
-            return new Info(null, level);;
+            return new Info(null, level);
 
         Info l = this.dfs(root.left, level + 1),
              r = this.dfs(root.right, level + 1);
@@ -28,6 +28,25 @@ public class Solution {
         if (l.height == r.height)
             return new Info(root, l.height);
         else if (l.height > r.height)
+            return l;
+        else
+            return r;
+    }
+
+    public TreeNode subtreeWithAllDeepestArr(TreeNode root) {
+        return (TreeNode) this.dfsArr(root, 0)[1];
+    }
+
+    private Object[] dfsArr(TreeNode root, int level) {
+        if (root == null)
+            return new Object[]{level, null};
+
+        Object[] l = this.dfsArr(root.left, level + 1),
+                r = this.dfsArr(root.right, level + 1);
+
+        if ((int) l[0] == (int) r[0])
+            return new Object[]{l[0], root};
+        else if ((int) l[0]  > (int) r[0])
             return l;
         else
             return r;
