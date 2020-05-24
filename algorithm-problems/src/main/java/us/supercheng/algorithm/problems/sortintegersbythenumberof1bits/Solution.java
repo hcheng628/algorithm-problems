deddf33d68ef477e7f64093098d39d8b72d76daa
Hongyu Cheng <chenghongyu628@gmail.com>
2020-05-23 23:42:34 -0400

    sort-integers-by-the-number-of-1-bits

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/sortintegersbythenumberof1bits/Solution.java
@@ -0,0 +1,36 @@
+package us.supercheng.algorithm.problems.sortintegersbythenumberof1bits;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+
+    public int[] sortByBits(int[] arr) {
+        int len = arr.length;
+        Integer[] list = new Integer[len];
+
+        for (int i=0; i<len; i++)
+            list[i] = arr[i];
+
+        Arrays.sort(list, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                int countA = 0,
+                        countB = 0;
+
+                for (int aa=a; aa>0;aa >>= 1)
+                    countA += (aa & 1);
+
+                for (int bb=b; bb>0;bb >>= 1)
+                    countB += (bb & 1);
+
+                return countA * 10000 + a > countB * 10000 + b ? 1 : -1;
+            }
+        });
+
+
+        for (int i=0; i<len; i++)
+            arr[i] = list[i];
+
+        return arr;
+    }
+}
