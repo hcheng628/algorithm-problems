4436b4e4de27c783217c6f5c07dd8405b132ef40
Hongyu Cheng <chenghongyu628@gmail.com>
2020-06-17 22:57:44 -0400

    generate-a-string-with-characters-that-have-odd-counts

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/generateastringwithcharactersthathaveoddcounts/Solution.java
@@ -0,0 +1,22 @@
+package us.supercheng.algorithm.problems.generateastringwithcharactersthathaveoddcounts;
+
+public class Solution {
+
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        if (n % 2 == 0) {
+            if (n == 2)
+                return "ab";
+
+            for (int i=1; i<n; i++)
+                sb.append("a");
+
+            sb.append("b");
+        } else
+            for (int i=0; i<n;i++)
+                sb.append("a");
+
+        return sb.toString();
+    }
+}
