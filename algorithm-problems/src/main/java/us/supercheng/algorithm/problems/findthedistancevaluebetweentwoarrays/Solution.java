644f192f0477877d0a3b50166cf35ce66f947040
Hongyu Cheng <chenghongyu628@gmail.com>
2020-07-05 21:42:06 -0400

    find-the-distance-value-between-two-arrays

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/findthedistancevaluebetweentwoarrays/Solution.java
@@ -0,0 +1,32 @@
+package us.supercheng.algorithm.problems.findthedistancevaluebetweentwoarrays;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int ret = 0,
+            len2 = arr2.length;
+
+        Arrays.sort(arr2);
+
+        for (int a : arr1) {
+            int idx = Arrays.binarySearch(arr2, a);
+            if (idx < 0) {
+                idx = -(++idx);
+                if (idx == 0) {
+                    if (Math.abs(a - arr2[idx]) > d)
+                        ret++;
+                } else if (idx == len2) {
+                    if (Math.abs(a - arr2[idx-1]) > d)
+                        ret++;
+                } else {
+                    if (Math.abs(a - arr2[idx]) > d && Math.abs(a - arr2[idx-1]) > d)
+                        ret++;
+                }
+            }
+        }
+
+        return ret;
+    }
+}
