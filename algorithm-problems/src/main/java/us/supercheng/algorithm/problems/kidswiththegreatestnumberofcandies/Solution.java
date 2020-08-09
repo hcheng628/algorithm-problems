64f8c7fa7d92d3ffd44fd30467b1f31ae57ef926
Hongyu Cheng <chenghongyu628@gmail.com>
2020-08-08 22:05:46 -0400

    kids-with-the-greatest-number-of-candies

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/kidswiththegreatestnumberofcandies/Solution.java
@@ -0,0 +1,30 @@
+package us.supercheng.algorithm.problems.kidswiththegreatestnumberofcandies;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        if (candies.length == 1)
+            return Arrays.asList(true);
+
+        int max = candies[0];
+        List<Boolean> ret = new ArrayList<>();
+
+        for (int c : candies)
+            if (c > max)
+                max = c;
+
+        for (int c : candies)
+            if (c + extraCandies >= max)
+                ret.add(true);
+            else
+                ret.add(false);
+
+        return ret;
+    }
+
+}
+//kids-with-the-greatest-number-of-candies
