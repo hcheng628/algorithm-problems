0f12b97598dfaffd799cc76a9d8b6ce1456ebc7c
Hongyu Cheng <chenghongyu628@gmail.com>
2020-08-16 23:13:10 -0400

    add iteratively approaoch

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/narytreepostordertraversal/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/narytreepostordertraversal/Solution.java
@@ -2,6 +2,7 @@ package us.supercheng.algorithm.problems.narytreepostordertraversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
 
@@ -15,6 +16,27 @@ public class Solution {
                 ret.addAll(this.postorder(n));
 
         ret.add(root.val);
+
+        return ret;
+    }
+
+    public List<Integer> postorderStack(Node root) {
+        List<Integer> ret = new ArrayList();
+
+        if (root != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(root);
+
+            while (!stack.isEmpty())
+                for (int i=0, len = stack.size(); i<len; i++) {
+                    Node n = stack.pop();
+                    ret.add(0, n.val);
+
+                    if (n.children != null)
+                        for (Node c : n.children)
+                            stack.push(c);
+                }
+        }
 
         return ret;
     }
