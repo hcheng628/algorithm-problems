28de7bc3866b9cc060cd46d4e9c59a05a7962f96
Hongyu Cheng <chenghongyu628@gmail.com>
2020-08-20 15:36:34 -0400

    find-words-that-can-be-formed-by-characters

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/findwordsthatcanbeformedbycharacters/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/findwordsthatcanbeformedbycharacters/Solution.java
@@ -1,4 +1,34 @@
 package us.supercheng.algorithm.problems.findwordsthatcanbeformedbycharacters;
 
 public class Solution {
+
+    public int countCharacters(String[] words, String chars) {
+        int ret = 0;
+        int[] map = new int[26],
+              tMap = new int [26];
+
+        for (char c : chars.toCharArray())
+            map[c - 'a']++;
+
+        for (String w : words) {
+            for (int i=0; i<26; i++)
+                if (tMap[i] != map[i])
+                    tMap[i] = map[i];
+
+            int len = w.length();
+            boolean yes = true;
+
+            for (int i=0; yes && i<len; i++) {
+                int idx = w.charAt(i) - 'a';
+                tMap[idx]--;
+                if (tMap[idx] < 0)
+                    yes = false;
+            }
+
+            if (yes)
+                ret += len;
+        }
+
+        return ret;
+    }
 }
