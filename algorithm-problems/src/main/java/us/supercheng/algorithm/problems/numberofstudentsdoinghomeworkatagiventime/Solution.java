6bd3de6e35b795be5e415da07930d3d71f80e9d6
Hongyu Cheng <chenghongyu628@gmail.com>
2020-08-23 08:30:33 -0400

    number-of-students-doing-homework-at-a-given-time

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/numberofstudentsdoinghomeworkatagiventime/Solution.java
@@ -0,0 +1,15 @@
+package us.supercheng.algorithm.problems.numberofstudentsdoinghomeworkatagiventime;
+
+public class Solution {
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int ret = 0;
+
+        for (int i=0, len=startTime.length; i<len; i++)
+            if (queryTime >= startTime[i] && queryTime <= endTime[i])
+                ret++;
+
+        return ret;
+    }
+
+}
