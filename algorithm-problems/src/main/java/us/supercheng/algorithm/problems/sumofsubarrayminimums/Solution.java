5ca8e14ff0f8d994c43428b518f1c979cbb09a18
Hongyu Cheng <chenghongyu628@gmail.com>
2020-08-29 22:52:50 -0400

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/sumofsubarrayminimums/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/sumofsubarrayminimums/Solution.java
@@ -20,4 +20,22 @@ public class Solution {
 
         return min;
     }
+
+    public int sumSubarrayMins(int[] A) {
+        int ret = 0;
+
+        for (int i=0, len = A.length; i<len; i++) {
+            int l = 0,
+                r = 0;
+
+            for (int idx=i-1; idx>-1 && A[idx]>A[i]; idx--)
+                l++;
+            for (int idx=i+1; idx<len && A[idx] >=A[i]; idx++)
+                r++;
+
+            ret = (ret + (++r) * (++l) * A[i]) % 1000000007;
+        }
+
+        return ret;
+    }
 }
