f7b918ffb5862bd9d8c590028312cda920cd22fe
Hongyu Cheng <chenghongyu628@gmail.com>
2020-09-07 14:56:11 -0400

    + different implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/snakeandladders/Solution.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/snakeandladders/Solution.java
@@ -12,6 +12,7 @@ public class Solution {
     private int[][] b;
     private Set<Integer> visited;
     private int ans = Integer.MAX_VALUE;
+    private Integer[] dfsMinDis;
 
     public int snakesAndLadders(int[][] board) {
         this.b = board;
@@ -19,8 +20,38 @@ public class Solution {
         this.size = board.length;
         this.end = this.size * this.size;
         this.ans = this.end;
-        this.BFS();
+        this.dfsMinDis = new Integer[this.end + 1];
+        this.dfsMinDis[1] = 0;
+
+        // this.BFS();
+        this.DFS(1, 0);
         return this.ans == this.end ? -1 : this.ans;
+    }
+
+    private void DFS(int from, int curr) {
+        if (from >= this.end) {
+            this.ans = Math.min(this.ans, curr);
+            return;
+        }
+
+        for (int steps=1; steps<=6; steps++) {
+            int next = from + steps;
+            if (next <= this.end) {
+                int[] pos = this.getP(next);
+                if (this.b[pos[0]][pos[1]] != -1) {
+                    next = this.b[pos[0]][pos[1]];
+                    if (this.dfsMinDis[next] == null || curr + 1 < this.dfsMinDis[next]) {
+                        this.dfsMinDis[next] = curr + 1;
+                        this.DFS(next, curr+1);
+                    }
+                } else {
+                    if (this.dfsMinDis[next] == null || curr + 1 < this.dfsMinDis[next]) {
+                        this.dfsMinDis[next] = curr + 1;
+                        this.DFS(next, curr+1);
+                    }
+                }
+            }
+        }
     }
 
     private void BFS() {
@@ -55,7 +86,7 @@ public class Solution {
 
     private int[] getP(int p) {
         int row = this.size - p / this.size,
-            col = p % this.size - 1;
+                col = p % this.size - 1;
 
         if (p % this.size != 0)
             row--;
