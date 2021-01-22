3d753e5e211471900520929b7ab0f027d65d17bf
Hongyu Cheng <chenghongyu628@gmail.com>
2021-01-22 17:57:40 -0500

    phone-number-mnemonics

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/phonenumbermnemonics/Program.java
@@ -0,0 +1,39 @@
+package us.supercheng.algorithm.problems.algo.phonenumbermnemonics;
+
+import java.util.*;
+
+public class Program {
+
+    public ArrayList<String> phoneNumberMnemonics(String phoneNumber) {
+        ArrayList<String> ret = new ArrayList<>();
+        int len = phoneNumber.length();
+        Map<Integer, List<Character>> map = new HashMap<>();
+
+        map.put(0, Arrays.asList('0'));
+        map.put(1, Arrays.asList('1'));
+        map.put(2, Arrays.asList('a', 'b', 'c'));
+        map.put(3, Arrays.asList('d', 'e', 'f'));
+        map.put(4, Arrays.asList('g', 'h', 'i'));
+        map.put(5, Arrays.asList('j', 'k', 'l'));
+        map.put(6, Arrays.asList('m', 'n', 'o'));
+        map.put(7, Arrays.asList('p', 'q', 'r', 's'));
+        map.put(8, Arrays.asList('t', 'u', 'v'));
+        map.put(9, Arrays.asList('w', 'x', 'y', 'z'));
+
+        this.helper(ret, phoneNumber, len, 0, map, new char[len]);
+        return ret;
+    }
+
+    private void helper(ArrayList<String> ret, String phone, int len, int idx, Map<Integer, List<Character>> map, char[] chars) {
+        if (idx == len) {
+            ret.add(String.valueOf(chars));
+            return;
+        }
+
+        for (Character c : map.get(phone.charAt(idx) - '0')) {
+            chars[idx] = c;
+            this.helper(ret, phone, len, idx+1, map, chars);
+        }
+    }
+
+}
