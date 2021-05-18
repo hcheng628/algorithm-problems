0f7c8de5089c7f1dab44fcfe18381f446956b4a7
Hongyu Cheng <chenghongyu628@gmail.com>
2021-05-17 22:36:24 -0400

    node-swap

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/nodeswap/Program.java
@@ -0,0 +1,38 @@
+package us.supercheng.algorithm.problems.algo.nodeswap;
+
+public class Program {
+
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    // Time: O(n) Space: O(1)
+    public LinkedList nodeSwap(LinkedList head) {
+        LinkedList ret = head;
+
+        if (head != null && head.next != null)
+            ret = head.next;
+
+        for (LinkedList curr = head, leftOver = null; curr != null && curr.next != null; ) {
+            LinkedList next = curr.next;
+
+            if (leftOver != null)
+                leftOver.next = next;
+            leftOver = curr;
+
+            LinkedList nextNext = next.next;
+            next.next = curr;
+            curr.next = nextNext;
+
+            curr = nextNext;
+        }
+
+        return ret;
+    }
+}
