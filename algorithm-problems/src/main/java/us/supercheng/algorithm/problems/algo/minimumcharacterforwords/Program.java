71efecbe72f5eab2c953df43d8b9d350f156952c
Hongyu Cheng <chenghongyu628@gmail.com>
2021-05-20 22:26:47 -0400

    minimum-character-for-words + different algorithm implementations

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/minimumcharacterforwords/Program.java
@@ -0,0 +1,33 @@
+package us.supercheng.algorithm.problems.algo.minimumcharacterforwords;
+
+import java.util.*;
+
+public class Program {
+
+    // Time: O(w*n) Space: O(n)
+    public String[] minimumCharactersForWords(String[] words) {
+        Map<Character, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+
+        for (String w : words) {
+            Map<Character, Integer> m = new HashMap<>();
+            for (char c : w.toCharArray()) {
+                m.put(c, m.getOrDefault(c, 0) + 1);
+                if (!map.containsKey(c) || map.get(c) < m.get(c))
+                    map.put(c, m.get(c));
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            char c = entry.getKey();
+            for (int i=0, len=entry.getValue(); i<len; i++)
+                list.add(Character.toString(c));
+        }
+
+        String[] ret = new String[list.size()];
+        for (int i=0, len=list.size(); i<len; i++)
+            ret[i] = list.get(i);
+
+        return ret;
+    }
+}
--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/nextgreaterelement/Program.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/nextgreaterelement/Program.java
@@ -1,5 +1,7 @@
 package us.supercheng.algorithm.problems.algo.nextgreaterelement;
 
+import java.util.*;
+
 public class Program {
 
     // Time: O(n^2) Space: O(n)
@@ -29,8 +31,39 @@ public class Program {
         return -1;
     }
 
-    // Time: O() Space: O()
+    // Time: O(n) Space: O(n)
     public int[] nextGreaterElement(int[] array) {
-        return null;
+        if (array == null || array.length < 1)
+            return new int[0];
+
+        int len = array.length;
+        int max = array[0];
+        int[] ret = new int[len];
+        Stack<Info> stack = new Stack<>();
+
+        for (int i=0; i<len; i++) {
+            max = Math.max(array[i], max);
+
+            while (!stack.isEmpty() && stack.peek().val < array[i])
+                ret[stack.pop().idx] = array[i];
+
+            stack.push(new Info(i, array[i]));
+        }
+
+        for (int i=0; i<len; i++)
+            for(; !stack.isEmpty() && (stack.peek().val == max || stack.peek().val < array[i]); stack.pop())
+                ret[stack.peek().idx] = stack.peek().val == max ? -1 : array[i];
+
+        return ret;
+    }
+
+    class Info {
+        int idx;
+        int val;
+
+        Info(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
     }
 }
