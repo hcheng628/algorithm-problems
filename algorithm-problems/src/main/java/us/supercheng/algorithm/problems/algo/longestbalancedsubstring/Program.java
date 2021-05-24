ebfaca48aa6c15d6069f47a5ad2651105a729471
Hongyu Cheng <chenghongyu628@gmail.com>
2021-05-23 21:44:49 -0400

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/longestbalancedsubstring/Program.java
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/algo/longestbalancedsubstring/Program.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Program {
 
     // Time: O(n^3) Space: O(1)
-    public int longestBalancedSubstring(String string) {
+    public int longestBalancedSubstring_Solution1(String string) {
         int ret = 0;
 
         for (int i=0, len=string.length(); i<len; i++)
@@ -30,6 +30,76 @@ public class Program {
             }
 
         return stack.isEmpty() ? count : 0;
+    }
+
+    // Time: O(n^2) Space: O(n)
+    public int longestBalancedSubstring_Solution2(String string) {
+        int ret = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        for (int i=0, len=string.length(); i<len; i++) {
+            char c = string.charAt(i);
+
+            if (c == '(')
+                stack.push(i);
+            else {
+                if (stack.isEmpty() || stack.peek() == -1)
+                    stack.push(i);
+                else {
+                    if (string.charAt(stack.peek()) == '(') {
+                        stack.pop();
+                        if (!stack.isEmpty())
+                            ret = Math.max(ret, i - stack.peek());
+                    } else
+                        stack.push(i);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    // Time: O(n) Space: O(1)
+    public int longestBalancedSubstring(String string) {
+        char[] chars = string.toCharArray();
+        return Math.max(this.helper3(chars, true), this.helper3(chars, false));
+    }
+
+    private int helper3(char[] chars, boolean isForward) {
+        int ret = 0;
+        int len =chars.length;
+        int openCount = 0;
+        int closeCount = 0;
+
+        if (isForward)
+            for (int i=0; i<len; i++) {
+                if (chars[i] == '(')
+                    openCount++;
+                else
+                    closeCount++;
+                if (openCount == closeCount)
+                    ret = Math.max(openCount * 2, ret);
+                else if (closeCount > openCount) {
+                    openCount = 0;
+                    closeCount = 0;
+                }
+            }
+        else
+            for (int i=len-1; i>-1; i--) {
+                if (chars[i] == ')')
+                    closeCount++;
+                else
+                    openCount++;
+                if (closeCount == openCount)
+                    ret = Math.max(closeCount * 2, ret);
+                else if (openCount > closeCount) {
+                    closeCount = 0;
+                    openCount = 0;
+                }
+            }
+
+        return ret;
     }
 
 }
