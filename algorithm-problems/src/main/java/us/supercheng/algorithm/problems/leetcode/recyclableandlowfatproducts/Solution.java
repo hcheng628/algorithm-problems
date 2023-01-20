ba6e3db75fb8b860e7b69fed19ffef4d95a75a34
Nikki Ma <hcheng98@gatech.edu>
2023-01-19 20:35:26 -0500

    solve 1757

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/recyclableandlowfatproducts/Solution.java
@@ -0,0 +1,9 @@
+package us.supercheng.algorithm.problems.leetcode.recyclableandlowfatproducts;
+
+public class Solution {
+
+    String SQL = "SELECT  product_id\n" +
+            "FROM Products\n" +
+            "WHERE low_fats = 'Y' AND recyclable = 'Y'";
+
+}
