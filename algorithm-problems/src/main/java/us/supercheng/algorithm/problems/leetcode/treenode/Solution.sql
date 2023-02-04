38db6fa899c3981b8804b599efc6b311b4325d08
Nikki Ma <hcheng98@gatech.edu>
2023-02-03 20:40:56 -0500

    add different implementation

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/treenode/Solution.sql
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/treenode/Solution.sql
@@ -1,3 +1,4 @@
+-- 1:
 WITH
     root AS (SELECT id, 'Root' AS type FROM Tree WHERE p_id IS NULL),
     leaf AS (SELECT id, 'Leaf' AS type FROM Tree
@@ -8,4 +9,10 @@ SELECT id, type FROM leaf
 UNION
 SELECT id, 'Inner' AS type FROM Tree
 WHERE
-        id NOT IN (SELECT id FROM root) AND id NOT IN (SELECT id FROM leaf);
+    id NOT IN (SELECT id FROM root) AND id NOT IN (SELECT id FROM leaf);
+
+-- 2:
+SELECT DISTINCT t1.id, IF(t1.p_id IS NULL, 'Root',
+                          IF (t2.p_id IS NULL, 'Leaf', 'Inner')) AS type
+FROM Tree AS t1
+LEFT JOIN Tree AS t2 ON t1.id = t2.p_id;
