3ffff5e98509c901bbfd81b39abfeaaa718712fb
Nikki Ma <hcheng98@gatech.edu>
2023-02-11 14:28:58 -0500

    solve 2236

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/rootequalssumofchildren/Solution.java
@@ -0,0 +1,26 @@
+package us.supercheng.algorithm.problems.leetcode.rootequalssumofchildren;
+
+import us.supercheng.algorithm.common.entity.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean checkTree(TreeNode root) {
+        return root.val == root.left.val + root.right.val;
+    }
+
+}
