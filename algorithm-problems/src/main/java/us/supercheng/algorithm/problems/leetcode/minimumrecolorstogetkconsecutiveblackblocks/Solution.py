9e099de1412b5ce40f5d4bc411ee5155796dc6a1
Nikki Ma <hcheng98@gatech.edu>
2023-02-20 20:32:53 -0500

    add different algorithm implementations

--- algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/minimumrecolorstogetkconsecutiveblackblocks/Solution.py
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/minimumrecolorstogetkconsecutiveblackblocks/Solution.py
@@ -6,16 +6,14 @@ class Solution(object):
         :rtype: int
         """
         ret = k
-        left = 0
         curr = 0
 
         for right in range(len(blocks)):
             if blocks[right] == 'W':
                 curr += 1
-            if right - left + 1 == k:
+            if right + 1 > k and blocks[right - k] == 'W':
+                curr -= 1
+            if right + 1 >= k:
                 ret = min(curr, ret)
-                if blocks[left] == 'W':
-                    curr -= 1
-                left += 1
 
         return ret
