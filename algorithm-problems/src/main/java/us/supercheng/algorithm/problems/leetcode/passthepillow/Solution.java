55e4469c3f53fe681ae4cca006e64b42de72c941
Nikki Ma <hcheng98@gatech.edu>
2023-03-06 20:40:39 -0500

    solve 2582

---
+++ algorithm-problems/src/main/java/us/supercheng/algorithm/problems/leetcode/passthepillow/Solution.java
@@ -0,0 +1,9 @@
+package us.supercheng.algorithm.problems.leetcode.passthepillow;
+
+class Solution {
+
+    public int passThePillow(int n, int time) {
+        return (time / (n - 1)) % 2 == 0 ? (time % (n - 1)) + 1 : n - (time % (n - 1));
+    }
+
+}
